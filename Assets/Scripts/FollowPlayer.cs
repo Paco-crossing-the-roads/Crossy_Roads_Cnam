b66f549ccffb1a4360cb8d834330e4ce50218a25
Vladimir9595 <vladisac@hotmail.it>
2024-04-07 18:46:06 +0200

    feat : camera follow the player

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothness;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, smoothness);
     }
 }
