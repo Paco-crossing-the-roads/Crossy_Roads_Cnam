04abf31edf1e2f594cf151d529ca9a056b80dce2
Vladimir9595 <vladisac@hotmail.it>
2024-04-07 20:48:55 +0200

    fix : rename scripts

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -2,20 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vehicle : MonoBehaviour
+public class MovingObject : MonoBehaviour
 {
     [SerializeField] private float speed;
     
     private void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.GetComponent<Player>() != null)
-        {
-            Destroy(collision.gameObject);
-        }
     }
 }
--- Assets/Scripts/MovingObjectSpawner.cs
+++ Assets/Scripts/MovingObjectSpawner.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VehicleSpawner : MonoBehaviour
+public class MovingObjectSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject vehicle; 
+    [SerializeField] private GameObject spawnObject; 
     [SerializeField] private Transform spawnPos;
     [SerializeField] private float minSeparationTime;
     [SerializeField] private float maxSeparationTime;
@@ -19,7 +19,7 @@ public class VehicleSpawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(minSeparationTime, maxSeparationTime));
-            GameObject go = Instantiate(vehicle, spawnPos.position, Quaternion.identity);
+            GameObject go = Instantiate(spawnObject, spawnPos.position, Quaternion.identity);
             if (!isRightSide)
             {
                 go.transform.Rotate(new Vector3(0, 180, 0));
