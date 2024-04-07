c5dfaccb4b5aeb86280797d5d1760f53e9b3fdb5
Vladimir9595 <vladisac@hotmail.it>
2024-04-07 20:12:33 +0200

    feat : add spawn vehicle and vehicle scripts

---
+++ Assets/Vehicle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.GetComponent<Player>() != null)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
