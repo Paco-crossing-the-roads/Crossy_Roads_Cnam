131655057c2b155f34270edf6e8222bcae95bb41
htregnagoCNAM <htregnago@cfai-formation.fr>
2024-04-15 09:54:24 +0200

    Add destroy object when out of bound #30

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -6,9 +6,16 @@ public class MovingObject : MonoBehaviour
 {
     [SerializeField] private float speed;
     public bool isLog;
-    
+
+    public float limiteZPositive = 25f;
+    public float limiteZNegative = -25f;
+
     private void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (transform.position.z > limiteZPositive || transform.position.z < limiteZNegative)
+        {
+            Destroy(gameObject);
+        }
     }
 }
