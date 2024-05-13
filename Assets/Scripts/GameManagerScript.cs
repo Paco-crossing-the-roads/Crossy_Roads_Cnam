67beff969ecd3257a362bb91b36e1f8c1822af95
htregnagoCNAM <htregnago@cfai-formation.fr>
2024-05-13 11:29:06 +0200

    Correction pièce

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -11,7 +11,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PauseManager.isPaused = false;
     }
 
     // Update is called once per frame
